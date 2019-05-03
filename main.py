13a06552ed96c13d9c7ff4a78b594416d3601679
clmendez <43482677+clmendez@users.noreply.github.com>
2019-05-02 17:11:41 -0700

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@
 import pyb
 import micropython
 import gc
+import pyplot
 
 import cotask
 import task_share
@@ -15,57 +16,46 @@ import print_task
 import controller
 import encoder
 import motor
-import pyb
-import micropython
-import utime
 
 # Allocate memory so that exceptions raised in interrupt service routines can
 # generate useful diagnostic printouts
 micropython.alloc_emergency_exception_buf (100)
 
+tim = pyb.Timer(1, freq= 1000)
+pinC0 = pyb.Pin (pyb.Pin.board.PC0, pyb.Pin.ANALOG)
+queue = task_share.Queue(float, 1000)
+adc = pyb.ADC(pinC0)
+pinC1 = pyb.Pin (pyb.Pin.board.PC1, pyb.Pin.OUT_PP)
+
+def main():
+    tim.callback(interrupt)
+    pinC1.high ()
+    
+    while queue.empty == False:
+        print(queue.get())
+    
+    pinC1.low()
+
+
+ #   pyplot.plot (queue.get(),time, 'g--')
+  #  pyplot.xlabel ("Time (fortnights)")
+   # pyplot.ylabel ("Height (furlongs)")
+    #pyplot.title ("Time Vs Height") 
+
+
+def interrupt():
+    
+    value = adc.read()
+    if queue.full() == False:
+        queue.put(value)
 # =============================================================================
 
 if __name__ == "__main__":
-
-    print ('\033[2JTesting scheduler in cotask.py\n')
-
-    # Create a share and some queues to test diagnostic printouts
-    share0 = task_share.Share ('i', thread_protect = False, name = "Share_0")
-    q0 = task_share.Queue ('B', 6, thread_protect = False, overwrite = False,
-                           name = "Queue_0")
-    q1 = task_share.Queue ('B', 8, thread_protect = False, overwrite = False,
-                           name = "Queue_1")
-
-    # Create the tasks. If trace is enabled for any task, memory will be
-    # allocated for state transition tracing, and the application will run out
-    # of memory after a while and quit. Therefore, use tracing only for 
-    # debugging and set trace to False when it's not needed
-    motor1_task = cotask.Task (task_calculation, name = 'motor1_task', priority = 1,
-                            period = 25, profile = True, trace = False)
-    motor2_task = cotask.Task (task_calculation, name = 'motor2_task', priority = 2,
-                            period = 25, profile = True, trace = False)
-    cotask.task_list.append (motor1_task)
-    cotask.task_list.append (motor2_task)
     
-    # A task which prints characters from a queue has automatically been
-    # created in print_task.py; it is accessed by print_task.put_bytes()
+    main()
+    
 
-    # Run the memory garbage collector to ensure memory is as defragmented as
-    # possible before the real-time scheduler is started
-    gc.collect ()
 
-    # Run the scheduler with the chosen scheduling algorithm. Quit if any 
-    # character is sent through the serial por
-    vcp = pyb.USB_VCP ()
-    while not vcp.any ():
-        cotask.task_list.pri_sched ()
-
-    # Empty the comm port buffer of the character(s) just pressed
-    vcp.read ()
-
-    # Print a table of task data and a table of shared information data
-    print ('\n' + str (cotask.task_list) + '\n')
-    print (task_share.show_all ())
-    print (control_task.get_trace ())
-    print ('\r\n')
-
+    
+    
+    
